67e85a1522cd1ea0134f30945059e63f58c386a4
Daniel Lopez <dlopez380@unab.edu.co>
2018-05-21 13:57:55 -0500

    Cambios con login

--- login.php
+++ login.php
@@ -1,19 +1,16 @@
 <?php
 	$con=mysql_connect("localhost", "root", "");
-
 	$mydb=mysql_select_db("software2");
-
 	if(isset($_POST["login"])){
 		$usuario=$_POST["user"];
-		$pass=$_POST["pass"];
-
-		$log = mysql_query("SELECT * FROM usuarios WHERE usuario='$usuario' AND pass='$pass'");
+        $pass=$_POST["pass"];
+        
+        $log = mysql_query("SELECT * FROM usuarios WHERE usuario='$usuario' AND pass='$pass'");
+        
 		/*$log2 = mysql_query("SELECT * FROM usuarios WHERE (usuario='$usuario' AND pass='$pass' AND tipousuario=empleado");
-		*/	
-
-
+        */	
+        
 		$row=mysql_fetch_array($log, MYSQL_ASSOC);
-
 				if (mysql_num_rows($log)>0){
 					if($row['tipousuario'] == 'empleado'){
 					/*if (mysql_num_rows($log2)>0){
