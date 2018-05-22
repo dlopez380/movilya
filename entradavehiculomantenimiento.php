4079aba283d0fa873c81970efd708204dbed8699
Daniel López <dlopez380@unab.edu.co>
2018-05-22 01:55:01 -0500

    push final

--- entradavehiculomantenimiento.php
+++ entradavehiculomantenimiento.php
@@ -4,22 +4,15 @@ $con = mysql_connect("localhost", "movilya", "Movilya2018");
 
 $mydb = mysql_select_db("software2");
 
-if (isset($_POST[ "vehiculomant" ])) {
+if (isset($_POST[ "valoresmant" ])) {
     $vehiculoenviar = $_POST[ "vehiculoenviar" ];
     $vehiculoparqueadero = $_POST[ "vehiculoparqueadero" ];
 
-    $log1 = mysql_query("SELECT $vehiculoenviar FROM parametrosmantenimiento WHERE id = 1");
-
-    while($result=mysql_fetch_array($log1, MYSQL_ASSOC)){
-         $result[$vehiculoenviar];
-    }
+    $log1 = mysql_query("INSERT INTO vehiculosmantenimiento (vehiculo,parqueadero) VALUES ('$vehiculoenviar','$vehiculoparqueadero')");
 
     $log2 = mysql_query("UPDATE parqueaderos SET $vehiculoenviar = $vehiculoenviar - 1 WHERE id = '$vehiculoparqueadero'");
+
 }
-
-?>
-
-<?php
 
 ?>
 
@@ -30,15 +23,45 @@ if (isset($_POST[ "vehiculomant" ])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
 </head>
 <body>
     <h2>MovilYa</h2>
     <h3>Empleado: reportar entrada de vehículo a mantenimiento</h3>
     </br>
-    <form>
+    <form method="post" action="entradavehiculomantenimiento.php">
+        1. Que vehículo quiere enviar a mantenimiento?
+        <select name="vehiculoenviar">
+            <option value="bicicletas">Bicicleta</option>
+            <option value="carros">Carro</option>
+            <option value="motos">Moto</option>
+        </select>
         </br>
-        2. El vehículo tiene <?php echo $result['$vehiculo'] ?>
+        2. Seleccione el parqueadero del vehículo que quiere mandar a mantenimiento... 
+        <input type="text" name="vehiculoparqueadero">
+        </br>
+            <!--2. El vehículo tiene -->         
+        <input type="submit" name="valoresmant" value="Mandar a mantenimiento">
     </form>
-    
+
+    <?php
+
+        $log3 = mysql_query("SELECT * FROM vehiculosmantenimiento");
+
+        while($result=mysql_fetch_array($log3, MYSQL_ASSOC)){
+            echo "<br/> Id mantenimiento: " . $result['id'];
+            echo "<br/> Vehiculo: " . $result['vehiculo'];
+            echo "<br/> Parqueadero proveniente: " . $result['parqueadero'];
+            echo "<br/>____________________________________";
+
+        }
+
+    ?>
+    <script>
+
+
+
+    </script>
 </body>
+
 </html>
---
+++ salidavehiculomantenimiento.php
@@ -0,0 +1,58 @@
+<?php
+
+$con = mysql_connect("localhost", "movilya", "Movilya2018");
+
+$mydb = mysql_select_db("software2");
+
+if (isset($_POST[ "valoresmant2" ])) {
+    $vehiculosacar = $_POST[ "vehiculosacar" ];
+
+    $log1 = mysql_query("DELETE FROM vehiculosmantenimiento WHERE vehiculosmantenimiento.id = $vehiculosacar");
+
+   
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+</head>
+<body>
+    <h2>MovilYa</h2>
+    <h3>Empleado: reportar salida de vehículo a mantenimiento</h3>
+    </br>
+    <form method="post" action="salidavehiculomantenimiento.php">
+        1. Que id de mantenimiento quiere eliminar (sacar vehiculo de mantenimiento)?
+        <input type="text" name="vehiculosacar">
+        </br>
+            <!--2. El vehículo tiene -->         
+        <input type="submit" name="valoresmant2" value="Sacar de mantenimiento">
+    </form>
+
+    <?php
+
+        $log3 = mysql_query("SELECT * FROM vehiculosmantenimiento");
+
+        while($result=mysql_fetch_array($log3, MYSQL_ASSOC)){
+            echo "<br/> Id mantenimiento: " . $result['id'];
+            echo "<br/> Vehiculo: " . $result['vehiculo'];
+            echo "<br/> Parqueadero proveniente: " . $result['parqueadero'];
+            echo "<br/>____________________________________";
+
+        }
+
+    ?>
+    <script>
+
+
+
+    </script>
+</body>
+
+</html>
